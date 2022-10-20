1972b57790f4d4cd138e66bd6517f8cf7355c60a
Ebuka Collins <52864306+ebukaracer@users.noreply.github.com>
2022-10-20 09:59:00 +0100

    Create PascalTriangle.cs
    Create a folder for C# language.
    Create PascalTriangle.cs.
    Move code file to it's respective folder.

---
+++ C#/PascalTriangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+/// <summary>
+/// Pascal’s Triangle is an arithmetic and geometric figure first imagined by Blaise Pascal.
+/// To build the triangle, start with ”1” at the top,
+/// then continue placing numbers below it in a triangular pattern.
+/// Each number is the numbers directly above it added together.
+/// </summary>
+internal class PascalTriangle
+{
+    private static void PascalTriangle(int rowCount = 5)
+    {
+        for (int row = 1; row <= rowCount; row++)
+        {
+            int tmp = 1;
+
+            for (int col = 1; col <= row; col++)
+            {
+                Console.Write($"{tmp} ");
+
+                tmp = tmp * (row - col) / col;
+            }
+
+            // Newline, per row.
+            Console.WriteLine();
+        }
+    }
+}
