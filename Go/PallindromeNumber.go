6a7b2cc6600690e09c3b19522bf5414b1d153f10
Ashutosh <70135665+Ashutosh-Ray@users.noreply.github.com>
2022-10-22 20:55:57 +0530

    Create PallindromeNumber.go

---
+++ Go/PallindromeNumber.go
@@ -0,0 +1,28 @@
+package main
+import "fmt"
+
+func main() {
+	var number,remainder,temp int
+	var reverse int = 0
+
+	fmt.Print("Enter any positive integer : ")
+	fmt.Scan(&number)
+
+	temp=number
+	for{
+		remainder = number%10
+		reverse = reverse*10 + remainder
+		number /= 10
+
+		if(number==0){
+			break 
+		}
+	}
+
+	if(temp==reverse){
+		fmt.Printf("%d is a Palindrome",temp)
+	}else{
+		fmt.Printf("%d is not a Palindrome",temp)
+	}
+
+}
